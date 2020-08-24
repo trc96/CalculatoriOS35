1e356471c84c5d5506538304eba6b04c0fbdd788
Todd Crandall <toddcrandall1996@gmail.com>
2020-08-24 17:22:11 -0600

    Finished Project

---
+++ CalculatoriOS35/CalculatoriOS35/CustomUI/StyleGuide.swift
@@ -0,0 +1,9 @@
+//
+//  StyleGuide.swift
+//  CalculatoriOS35
+//
+//  Created by Todd Crandall on 8/24/20.
+//  Copyright © 2020 Todd Crandall. All rights reserved.
+//
+
+import Foundation
---
+++ CalculatoriOS35/CalculatoriOS35/CustomUI/TipButton.swift
@@ -0,0 +1,9 @@
+//
+//  TipButton.swift
+//  CalculatoriOS35
+//
+//  Created by Todd Crandall on 8/24/20.
+//  Copyright © 2020 Todd Crandall. All rights reserved.
+//
+
+import Foundation
